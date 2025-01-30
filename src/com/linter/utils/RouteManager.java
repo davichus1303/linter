c252d4aff426763dde7109926c39798a7008d600
david cano <david.cano@netlogistik.com>
2025-01-29 19:36:52 -0600

    fix ident and add description

--- src/com/linter/utils/RouteManager.java
+++ src/com/linter/utils/RouteManager.java
@@ -6,22 +6,30 @@ import java.util.Scanner;
 
 public class RouteManager {
 
-    public static Path obtenerRuta() {
-		String defaultRoute = "/home/david/Tep/";
-        Scanner scanner = new Scanner(System.in);
+  /**
+   * Obtains the path to a project directory.
+   * 
+   * This method prompts the user to input the name of a project folder within a default directory.
+   * If the user provides an empty input, it then prompts the user to input the full path to the project directory.
+   * 
+   * @return A Path object representing the project directory.
+   */
+  public static Path obtenerRuta() {
+    String defaultRoute = "/home/david/Tep/";
+    Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Escribe el nombre del proyecto en la carpeta default, enter para asignar ruta: ");
-        
-        String projectFolder = scanner.nextLine();
-        if (projectFolder.equals("")) {
-        	System.out.print("Escribe la ruta del proyecto:");
-        	projectFolder = scanner.nextLine();
-        } else {
-        	projectFolder = defaultRoute + projectFolder;
-        }
-        Path route = Paths.get(projectFolder);
-        scanner.close();
-        
-        return route;
+    System.out.print("Escribe el nombre del proyecto en la carpeta default, enter para asignar ruta: ");
+    
+    String projectFolder = scanner.nextLine();
+    if (projectFolder.equals("")) {
+      System.out.print("Escribe la ruta del proyecto:");
+      projectFolder = scanner.nextLine();
+    } else {
+      projectFolder = defaultRoute + projectFolder;
     }
+    Path route = Paths.get(projectFolder);
+    scanner.close();
+    
+    return route;
+  }
 }
